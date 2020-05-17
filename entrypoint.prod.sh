c3115698e02ce43ed48d2cdb68bd2236ad17849f
Ivan Kuzel <ivankuzel@gmail.com>
2020-05-17 18:05:46 -0300

    updating entrypoint and pillow version

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -1,19 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 python manage.py migrate                  # Apply database migrations
 python manage.py collectstatic --noinput  # Collect static files
 
 # Prepare log files and start outputting logs to stdout
-touch /srv/logs/gunicorn.log
-touch /srv/logs/access.log
-tail -n 0 -f /srv/logs/*.log &
 
-# Start Gunicorn processes
-echo Starting Gunicorn.
-exec gunicorn recetapp.wsgi:application \
-    --name recetapp \
-    --bind 0.0.0.0:8000 \
-    --workers 3 \
-    --log-level=info \
-    --log-file=/srv/logs/gunicorn.log \
-    --access-logfile=/srv/logs/access.log \
-    "$@"
+# touch /srv/logs/gunicorn.log
+# touch /srv/logs/access.log
+# tail -n 0 -f /srv/logs/*.log &
+
+# # Start Gunicorn processes
+# echo Starting Gunicorn.
+# exec gunicorn recetapp.wsgi:application \
+#     --name recetapp \
+#     --bind 0.0.0.0:8000 \
+#     --workers 3 \
+#     --log-level=info \
+#     --log-file=/srv/logs/gunicorn.log \
+#     --access-logfile=/srv/logs/access.log \
+#     "$@"
